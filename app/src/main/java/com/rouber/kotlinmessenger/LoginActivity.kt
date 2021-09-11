ff23af5a53669829c90d01b8a41b928e90f1d8bd
RouberR <Rouber1998@gmail.com>
2021-09-11 18:15:09 +0300

    Version 0.7

--- app/src/main/java/com/rouber/kotlinmessenger/LoginActivity.kt
+++ app/src/main/java/com/rouber/kotlinmessenger/LoginActivity.kt
@@ -3,7 +3,9 @@ package com.rouber.kotlinmessenger
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity() {
@@ -11,15 +13,33 @@ class LoginActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+
         login_button_login.setOnClickListener {
-            val email = email_text_login.text.toString()
-            val password = password_text_login.text.toString();
-            Log.d("LoginActivity", "Email is $email\n Password is $password")
+            performLogin()
         }
+
+
         back_regiscter_text.setOnClickListener {
             finish()
         }
 
 
+    }
+
+
+    private fun performLogin(){
+            val email = email_text_login.text.toString()
+            val password = password_text_login.text.toString();
+            Log.d("LoginActivity", "Email is $email\n Password is $password")
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if (!it.isSuccessful) return@addOnCompleteListener
+                    Log.d("Main", "Пользователь успешно вошел! id =  ${it.result!!.user!!.uid}")
+                }
+                .addOnFailureListener{
+                    Log.d("Main", "Ошибка при входе пользователя ${it.message}")
+                    Toast.makeText(this, "Ошибка при входе пользователя ${it.message}", Toast.LENGTH_SHORT).show()
+                }
     }
 }
---
+++ app/src/main/java/com/rouber/kotlinmessenger/RegisterActivity.kt
@@ -0,0 +1,131 @@
+package com.rouber.kotlinmessenger
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.activity_register.*
+import java.util.*
+
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        register_button_register.setOnClickListener {
+            performRegister()
+        }
+
+
+        already_have_account_textview.setOnClickListener {
+            Log.d("MainActivity", "Click ")
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        photo_button_register.setOnClickListener {
+            Log.d("RegisterActivity", "Click button photo")
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 0)  //разобраться с registerForActivityResult
+
+        }
+
+    }
+
+    var selectedPhotoUri: Uri? = null
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 0 && resultCode == RESULT_OK && data != null) {
+            Log.d("RegisterActivity", "Photo was selected")
+
+            selectedPhotoUri = data.data
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedPhotoUri)
+
+
+            selectphoto_image_register.setImageBitmap(bitmap)
+            photo_button_register.alpha=0f
+          //  val bitmapDrawable = BitmapDrawable(bitmap)
+         //   photo_button_register.setBackgroundDrawable(bitmapDrawable)
+        }
+    }
+
+    private fun performRegister() {
+
+
+        val email = email_edittext_register.text.toString()
+        val password = password_edittext_register.text.toString()
+
+        Log.d("MainActivity", "Email is $email")
+        Log.d("MainActivity", "Password is $password")
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Пожалуйста ведите Email и Password", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+
+                Log.d(
+                    "RegisterActivity",
+                    "Пользователь создан успешно! id =  ${it.result!!.user!!.uid}"
+                )
+
+                uploadImageToFirebaseStorage()
+
+            }
+            .addOnFailureListener {
+                Log.d("RegisterActivity", "Ошибка при создание пользователя ${it.message}")
+                Toast.makeText(
+                    this, "Ошибка при создание пользователя ${it.message}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+    }
+
+    private fun uploadImageToFirebaseStorage() {
+        if (selectedPhotoUri == null) return
+
+        val filename = UUID.randomUUID().toString()
+        val ref = FirebaseStorage.getInstance().getReference("/images/$filename")
+        ref.putFile(selectedPhotoUri!!)
+            .addOnSuccessListener { it ->
+                Log.d("RegisterActivity", "Успешно добавлено изображение ${it.metadata?.path}")
+
+                ref.downloadUrl.addOnSuccessListener {
+                    Log.d("RegisterActivity", "File location $it")
+
+                    saveUserToFiredaseDatabase(it.toString())
+                }
+            }
+            .addOnFailureListener {
+
+            }
+    }
+
+    private fun saveUserToFiredaseDatabase(profileImageUrl: String) {
+        val uid = FirebaseAuth.getInstance().uid ?: ""
+        val ref = FirebaseDatabase.getInstance().getReference("/users/$uid")
+
+        val user = User(uid, username_edittext_register.text.toString(), profileImageUrl)
+
+        ref.setValue(user)
+            .addOnSuccessListener {
+                Log.d("RegisterActivity", "Пользователь сохранен в базе данных")
+            }
+    }
+}
+class User(val uid:String, val username: String, val profileImageUrl: String)
